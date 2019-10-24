c43c6be1fec3da57185252e825ab5ac1f0306bb4
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-24 02:32:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
   holiday.each do |key, value|
     formatted_holiday = key.to_s.split('_')
-    final_formatted_holiday = formatted_holiday.map {|word|}
+    final_formatted_holiday = formatted_holiday.map {|word| word.capitalize
     puts "#{season}: #{holiday}: #{supplies}"
     end
   end
