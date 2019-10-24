89980b2c5a11b12b6279655bf0d65a10d81638df
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-24 02:40:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |holiday|
+  holiday_hash.each do |season, holiday|
     binding.pry
     if value == "BBQ"
       puts key
